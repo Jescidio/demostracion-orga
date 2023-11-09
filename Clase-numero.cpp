180a8038b6056a22ebc94c78ead784b2616a5623
Jescidio <jedasemo@gmail.com>
2023-11-09 00:04:07 -0600

    Create Clase-numero.cpp
    Creé una clase Numero y una función main para probarla

---
+++ Clase-numero.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+class Numero{
+private:
+  int num;
+public:
+Numero(){
+  num=0;
+}
+~Numero{
+}
+int getNum(){
+  return num;
+}
+void setNum(int n_){
+num=n_;
+}
+bool esPar(){
+  if(num%2 == 0)
+    return true;
+  return false;
+}
+};
+
+int main(){
+
+  Numero* n = new Numero;
+  n->setNum(40);
+
+  if(n->esPar)
+    cout<<n->getNum()<<" es par"<<endl;
+  else
+    cout<<n->getNum()<<" es impar"<<endl;
+  
+  return 0;
+}
